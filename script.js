c0f5ddb455125bb4133d4a99200f210e47d26f74
vebryanto <vebrypp@gmail.com>
2022-08-24 14:43:55 +0700

    post sample 1

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    const bannerContent = document.querySelectorAll('.banner-content');
+    const scrollTop = document.querySelector('.scroll-top');
+    const content = document.querySelectorAll('.content .content-wrapper #content');
+    const windowY = window.innerHeight;
+
+    // Scroll Event
+    document.addEventListener('scroll', function() {
+        const y = window.scrollY;
+
+        // Show content
+        content.forEach(function(i) {
+            const rect = i.getBoundingClientRect();
+            const rectY = rect.top.toFixed();
+
+            if( rectY < ( windowY / 2 ) && rectY > (windowY / 2) - ( windowY / 2 / 3 ) ) {
+                if(i.classList.contains('hide')) return (i.classList.replace('hide', 'show'));
+                i.classList.add('show');
+            } else {
+                i.classList.replace('show', 'hide');
+            };
+        });
+
+        // Show and hide navbar and scrolltop icon
+        if( y > 0 ) {
+            navbar.classList.add('hide');
+            scrollTop.classList.replace('hideIcon', 'showIcon');
+        };
+        if( y == 0 ) {
+            navbar.classList.remove('hide');
+            scrollTop.classList.replace('showIcon', 'hideIcon');
+        };
+    });
+
+    // Scroll top
+    scrollTop.addEventListener('click', function() {
+        document.documentElement.scrollTop = 0;
+    });
+
+    // SLide Banner
+    let i = 0
+    setInterval(function() {
+        if( i == 2 ) {
+            bannerContent[i-1].classList.replace('content-left','content-right');
+            bannerContent[i].classList.replace('content-middle', 'content-left');
+            bannerContent[0].classList.replace('content-right', 'content-middle');
+            i = 0;
+        } else if( i == 0 ) {
+            bannerContent[bannerContent.length-1].classList.replace('content-left', 'content-right');
+            bannerContent[i].classList.replace('content-middle', 'content-left');
+            bannerContent[i+1].classList.replace('content-right', 'content-middle');
+            i++;
+        } else {
+            bannerContent[i-1].classList.replace('content-left','content-right');
+            bannerContent[i].classList.replace('content-middle', 'content-left');
+            bannerContent[i+1].classList.replace('content-right', 'content-middle');
+            i++;
+        };
+    }, 3000);
+});
