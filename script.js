946a51300ef4916d1108af6f30db31a37332af6b
vebryanto <vebrypp@gmail.com>
2022-08-24 15:43:35 +0700

    add effect side dot active

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const bannerContent = document.querySelectorAll('.banner-content');
     const scrollTop = document.querySelector('.scroll-top');
     const content = document.querySelectorAll('.content .content-wrapper #content');
+    const dotSideNavigation = document.querySelectorAll('.side-navigation-dot');
     const windowY = window.innerHeight;
 
     // Scroll Event
     document.addEventListener('scroll', function() {
         const y = window.scrollY;
+
+        // Dot side navigation
+        dotSideNavigation.forEach(function(i) {
+            const rect = i.getBoundingClientRect();
+            const rectY = rect.top.toFixed();
+
+            if( rectY < ( windowY / 2 ) && rectY > (windowY / 2) - ( windowY / 2 / 3 ) ) {
+                if( i.classList.contains('active') == false ) {
+                    i.classList.add('active');
+                };
+            } else {
+                i.classList.remove('active');
+            };
+        });
 
         // Show content
         content.forEach(function(i) {
